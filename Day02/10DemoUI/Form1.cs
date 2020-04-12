c9283fed70584d14bf7b603e9f942737aa7f218f
Diego Acero <Diego.Acero@endava.com>
2020-04-12 08:09:11 -0500

    Async training
    Day02 examples.

---
+++ Day02/10DemoUI/Form1.cs
@@ -0,0 +1,88 @@
+﻿namespace _10DemoUI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+    using Core;
+
+    public partial class Form1 : Form
+    {
+        private const string Path = "..\\..\\..\\cars.csv";
+        private delegate void SafeCallDelegate(string text);
+
+        public Form1()
+        {
+            this.InitializeComponent();
+        }
+
+        private void GetDataBtn_Click(object sender, EventArgs e)
+        {
+            this.Log("start to process file");
+
+            Task task1 = Task.Factory.StartNew(() =>
+            {
+                return this.ProcessCarsFile(Path).ToList();
+
+            }).ContinueWith(ant =>
+            {
+                if (ant.Exception != null)
+                {
+                    Console.WriteLine(ant.Exception.Message);
+                }
+                else
+                {
+                    this.DisplayCars(ant.Result);
+                    this.Log($"finish to process file. {ant.Result.Count()} cars downloaded");
+                }
+            });
+
+            task1.Wait();
+        }
+
+        private void DisplayCars(List<Car> cars)
+        {
+            foreach (var car in cars)
+            {
+                this.AppendToContent(car.ToString());
+            }
+        }
+
+        private IEnumerable<Car> ProcessCarsFile(string filePath)
+        {
+            var cars = new List<Car>(600);
+            var lines = File.ReadAllLines(filePath).Skip(2);
+
+            foreach (var line in lines)
+            {
+                cars.Add(Car.Parse(line));
+            }
+
+            Thread.Sleep(TimeSpan.FromSeconds(3)); // simulate some work
+
+            return cars;
+        }
+
+        public void Log(string s)
+        {
+            this.logTbx.AppendText($"{DateTime.Now} - {s}{Environment.NewLine}");
+        }
+
+        public void AppendToContent(string s)
+        {
+            if (this.contentTxb.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(AppendToContent);
+                contentTxb.Invoke(d, new object[] { s });
+            }
+            else
+            {
+
+                this.contentTxb.AppendText($"{s}{Environment.NewLine}");
+            }
+        }
+    }
+}
---
+++ Day02/JSon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JSon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
