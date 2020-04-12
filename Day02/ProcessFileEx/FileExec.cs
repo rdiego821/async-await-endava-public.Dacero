8fc4fcd34aa0bb50494cee8719362bc27791cc92
Diego Acero <Diego.Acero@endava.com>
2020-04-12 08:47:48 -0500

    Async training
    Add home exercise - day 2

---
+++ Day02/ProcessFileEx/FileExec.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Security.Permissions;
+using System.Threading;
+
+namespace ProcessFileEx
+{
+    public class FileExec
+    {
+        private const string Path = @"C:\AsyncTraining\Exercise1\async-await-endava-public.Dacero\Day02\MyFiles\";
+        public Queue<string> files = new Queue<string>();
+        public int NumberOfFiles = 0;
+        static SemaphoreSlim _sem = new SemaphoreSlim(4);
+
+        public void ReadFile(string FileName)
+        {
+            using (StreamReader sr = File.OpenText(Path + FileName))
+            {
+                string s;
+                Console.WriteLine($"File name: {FileName} - Content: ");
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+
+        // Consumer
+        public void ProcessFiles()
+        {
+            _sem.Wait();
+            while (files.Count > 0)
+                ReadFile(files.Dequeue());
+            _sem.Release();
+        }
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        public void WatchDirectory()
+        {
+            using (FileSystemWatcher watcher = new FileSystemWatcher())
+            {
+                watcher.Path = Path;
+
+                watcher.NotifyFilter = NotifyFilters.LastAccess
+                                     | NotifyFilters.LastWrite
+                                     | NotifyFilters.FileName
+                                     | NotifyFilters.DirectoryName;
+
+                watcher.Filter = "*.txt";
+                watcher.Created += EnqueueFile;
+                watcher.EnableRaisingEvents = true;
+                while (NumberOfFiles <= 10) ;
+            }
+        }
+
+        // Define the event handlers. Producer
+        private void EnqueueFile(object source, FileSystemEventArgs e)
+        {
+            files.Enqueue(e.Name);
+            NumberOfFiles++;
+        }
+    }
+}
---
+++ Day02/ProcessFileEx/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ProcessFileEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileExec file = new FileExec();
+            file.WatchDirectory();
+
+            Task t1 = new Task(file.ProcessFiles);
+            t1.Start();
+            t1.Wait();
+        }
+    }
+}
